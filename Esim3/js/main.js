febd24b1c18a48c4d1f9a38167ce20fa662ba283
delosait-ru <info@delosait.ru>
2021-10-26 13:17:38 +0300

    remarks

--- Esim3/js/main.js
+++ Esim3/js/main.js
@@ -1,36 +1,38 @@
+"use strict";
+
+console.log('global');
+"use strict";
+
+console.log('maxgraph');
+"use strict";
+
 /**
  * название функции
  *
  * @param {number} first - первое число
  * @returns {number}
  */
+
 /*Мега меню*/
-
 $(function () {
-
-    $(".sf-menu").superfish({
-            delay: 200,
-            speed: "fast",
-            cssArrows: false
-        })
-        .after("<div id='mobile-menu'>").clone().appendTo("#mobile-menu");
-    $("#mobile-menu").find("*").attr("style", "");
-    $("#mobile-menu").children("ul").removeClass("sf-menu")
-        .parent().mmenu({
-            extensions: ['widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black'],
-            navbar: {
-                title: "Меню"
-
-            }
-        });
-
-    $(".toggle-mnu").click(function () {
-        $(this).addClass("on");
-
-    });
-
-    var api = $("#mobile-menu").data("mmenu");
-    api.bind("closed", function () {
-        $(".toggle-mnu").removeClass("on");
-    });
+  $(".sf-menu").superfish({
+    delay: 200,
+    speed: "fast",
+    cssArrows: false
+  }).after("<div id='mobile-menu'>").clone().appendTo("#mobile-menu");
+  $("#mobile-menu").find("*").attr("style", "");
+  $("#mobile-menu").children("ul").removeClass("sf-menu").parent().mmenu({
+    extensions: ['widescreen', 'theme-white', 'effect-menu-slide', 'pagedim-black'],
+    navbar: {
+      title: "Меню"
+    }
+  });
+  $(".toggle-mnu").click(function () {
+    $(this).addClass("on");
+  });
+  var api = $("#mobile-menu").data("mmenu");
+  api.bind("closed", function () {
+    $(".toggle-mnu").removeClass("on");
+  });
 });
+//# sourceMappingURL=main.js.map
